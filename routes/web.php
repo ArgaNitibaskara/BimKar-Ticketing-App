0e4dbcd56245eff04932ec2521bba069aff488a0
ArgaNitibaskara <111202214642@mhs.dinus.ac.id>
2026-01-16 11:56:57 +0700

    Modul 9

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,9 @@ Route::middleware('auth')->group(function () {
 
         // Category Management
         Route::resource('categories', CategoryController::class);
+
+        // Event Management
+        Route::resource('events', EventController::class);
     });
 });
 
